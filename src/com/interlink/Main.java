76bfbebe66371b2baddc635f5c1377eb58e127ea
OleksiiT-in6k <OleksiiT@softwareplanetgroup.com>
2016-07-11 15:06:09 +0300

    refactor main

--- src/com/interlink/Main.java
+++ src/com/interlink/Main.java
@@ -9,16 +9,12 @@ import java.time.Year;
 
 public class Main {
 
-    public static void showCalendar(Month month, Year year) {
-        CalendarView.printCalendar(LocalDate.now(), new CalendarModel(month, year).getDates());
-    }
-
     public static void main(String[] args) {//Parameter should be a name of the month(all letters are capital), like FEBRUARY, or empty.
         Year year = Year.now();
         Month month = LocalDate.now().getMonth();
         if (!(args.length == 0)) {
             month = Month.valueOf(args[0]);
         }
-        showCalendar(month, year);//The main method. Print calendar for month by month and year.
+        CalendarView.printCalendar(LocalDate.now(), new CalendarModel(month, year).getDates());
     }
 }
