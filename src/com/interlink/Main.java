2b37ef2ad2b776537244cb31d97d4b4b57309055
OleksiiT-in6k <OleksiiT@softwareplanetgroup.com>
2016-07-12 14:31:27 +0300

    reformat+delete unused methods

--- src/com/interlink/Main.java
+++ src/com/interlink/Main.java
@@ -2,14 +2,13 @@ package com.interlink;
 
 import com.interlink.model.CalendarModel;
 import com.interlink.view.CalendarView;
-
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.Year;
 
 public class Main {
 
-    public static void main(String[] args) {//Parameter should be a name of the month(all letters are capital), like FEBRUARY, or empty.
+    public static void main(String[] args) {
         Year year = Year.now();
         Month month = LocalDate.now().getMonth();
         if (!(args.length == 0)) {
