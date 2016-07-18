cec4c8b3a7a324a006713f4bd28c2933177a49b9
OleksiiT-in6k <OleksiiT@softwareplanetgroup.com>
2016-07-18 09:11:29 +0300

    Delete comments.

--- src/com/interlink/Main.java
+++ src/com/interlink/Main.java
@@ -1,7 +1,5 @@
 package com.interlink;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.time.LocalDate;
 import java.time.YearMonth;
 
@@ -10,40 +8,9 @@ public class Main {
     static CalendarFacade calendarFacade;
 
     public static void main(String[] args) {
-//        Year year = Year.now();
-//        Month month = LocalDate.now().getMonth();
-//        String secondParameter;
-//        secondParameter = args[1];
-//        calendarFacade = new CalendarFacade(month, year, "console");
-//        switch (secondParameter) {
-//            case "console": {
-//                printInConsole(calendarFacade.generateCalendar(LocalDate::now));
-//                break;
-//            }
-//            case "Html": {
-//                printInHtml(calendarFacade.generateCalendar(LocalDate::now));
-//                break;
-//            }
-//
-//        }
 
         Controller controller = new Controller(YearMonth.of(2016, 7), LocalDate.now());
         controller.run();
 
-    }
-
-    protected static void printInConsole(String text) {
-        System.out.println(text);
-    }
-
-
-    protected static void printInHtml(String text) {
-        try {
-            BufferedWriter writeToHtml = new BufferedWriter(new FileWriter("calendar.html"));
-            writeToHtml.write(text);
-            writeToHtml.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
--- test/com/interlink/ControllerTest.java
+++ test/com/interlink/ControllerTest.java
@@ -10,14 +10,13 @@ import org.mockito.Mockito;
 import java.time.LocalDate;
 import java.time.YearMonth;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.spy;
 
 public class ControllerTest {
     @Test
     public void test() throws Exception {
         Controller controller = new Controller(YearMonth.of(2016, 4), LocalDate.of(2016, 4, 5));
         Controller spy = spy(controller);
-        //when(spy.getCommand()).thenReturn("s");
         Mockito.doReturn("s").when(spy).getCommand();
     }
 }
